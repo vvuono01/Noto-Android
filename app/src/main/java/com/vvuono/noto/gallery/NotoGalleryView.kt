9deed937f999ffcc4d8dbb20cf7124ba43d15159
Vince Vuono <vvuono01@gmail.com>
2025-11-18 11:44:26 -0500

    Implement FAB on Gallery screen

--- app/src/main/java/com/vvuono/noto/gallery/NotoGalleryView.kt
+++ app/src/main/java/com/vvuono/noto/gallery/NotoGalleryView.kt
@@ -1,24 +1,48 @@
 package com.vvuono.noto.gallery
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.vvuono.noto.ui.theme.NotoTheme
 
 @Composable
 fun NotoGalleryView() {
     NotoTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        val context = LocalContext.current
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { onFloatingActionButtonClick(context) },
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary,
+                ) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "New Noto")
+                }
+            },
+        ) { innerPadding ->
             Greeting(
                 name = "Android",
                 modifier = Modifier.padding(innerPadding)
             )
         }
     }
+}
+
+private fun onFloatingActionButtonClick(context: Context) {
+    Toast.makeText(context, "Make a new Noto!", Toast.LENGTH_SHORT).show()
 }
 
 @Composable
--- app/src/main/java/com/vvuono/noto/ui/theme/Theme.kt
+++ app/src/main/java/com/vvuono/noto/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.vvuono.noto.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -37,7 +36,7 @@ private val LightColorScheme = lightColorScheme(
 fun NotoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
