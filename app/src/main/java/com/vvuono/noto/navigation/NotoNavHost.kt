75b07c21ce8e759ef6fea5ba951e970d6556669a
Vince Vuono <vvuono01@gmail.com>
2025-11-24 13:26:21 -0500

    Implement screen transition animations

--- app/src/main/java/com/vvuono/noto/navigation/NotoNavHost.kt
+++ app/src/main/java/com/vvuono/noto/navigation/NotoNavHost.kt
@@ -1,5 +1,9 @@
 package com.vvuono.noto.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -8,6 +12,8 @@ import androidx.navigation.compose.composable
 import com.vvuono.noto.create.CreateNotoView
 import com.vvuono.noto.data.ui.NotoScreen
 import com.vvuono.noto.gallery.NotoGalleryView
+
+private const val TRANSITION_DURATION_MILLIS = 250
 
 @Composable
 fun NotoNavHost(
@@ -18,6 +24,30 @@ fun NotoNavHost(
         navController = navController,
         startDestination = NotoScreen.Gallery.name,
         modifier = modifier,
+        enterTransition = {
+            slideIntoContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                animationSpec = tween(TRANSITION_DURATION_MILLIS),
+            )
+        },
+        exitTransition = {
+            scaleOut(
+                targetScale = 0.9f,
+                animationSpec = tween(TRANSITION_DURATION_MILLIS),
+            )
+        },
+        popEnterTransition = {
+            scaleIn(
+                initialScale = 0.9f,
+                animationSpec = tween(TRANSITION_DURATION_MILLIS),
+            )
+        },
+        popExitTransition = {
+            slideOutOfContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                animationSpec = tween(TRANSITION_DURATION_MILLIS),
+            )
+        }
     ) {
         composable(route = NotoScreen.Gallery.name) {
             NotoGalleryView(navController)
