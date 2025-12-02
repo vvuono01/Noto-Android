252e7480bb0bbbfc7ab4da00ce367c6bcea1d0d2
Vince Vuono <vvuono01@gmail.com>
2025-12-02 01:25:00 -0500

    Display image on view page

--- app/src/main/java/com/vvuono/noto/view/ViewNotoView.kt
+++ app/src/main/java/com/vvuono/noto/view/ViewNotoView.kt
@@ -1,10 +1,16 @@
 package com.vvuono.noto.view
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 fun ViewNotoView() {
@@ -12,6 +18,13 @@ fun ViewNotoView() {
     val notoPhotoUri by viewModel.notoPhotoUri.collectAsStateWithLifecycle()
 
     Scaffold { innerPadding ->
-        // TODO: Display image
+        val context = LocalContext.current
+        AsyncImage(
+            model = ImageRequest.Builder(context)
+                .data(notoPhotoUri)
+                .build(),
+            contentDescription = "Noto",
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
